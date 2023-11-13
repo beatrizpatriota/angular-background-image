4d166263fb84bbadbb13e1378616138f447e7951
Beatriz de Luca Patriota <beatriz.lp@alumni.usp.br>
2023-11-13 19:16:30 -0300

    Changed first page

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicComponent } from './public.component';
-import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from '../login/login.component';
 
 const routes: Routes = [
   { path: '', component: PublicComponent, children: [
-    { path: '', component: LandingComponent}
+    { path: '', component: LoginComponent}
   ]}
 ];
 
